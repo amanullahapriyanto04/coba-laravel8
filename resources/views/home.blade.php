cbd8e222632645294649a2531c6788f2a527f04e
Amanullah Apriyanto <amanullahapriyanto2001@gmail.com>
2022-05-10 12:10:58 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>AMAN Blog</title>
+</head>
+
+<body>
+    <section class="container">
+        <!-- img -->
+        <article class="img-block">
+            <img src="" alt="avatar" class="avatar" />
+            <h3>Amanullah Apriyanto</h3>
+        </article>
+
+        <!-- links -->
+        <article class="links-block">
+            <h5>Informatics Engineering</h5>
+
+            <div class="links">
+                <a href="https://google.com" class="link" target="_blank">website <i class="fas fa-globe"></i></a>
+
+                <a href="https://www.youtube.com/" class="link" target="_blank">youtube <i class="fab fa-youtube"></i></a>
+
+                <a href="https://github.com/amanullahapriyanto04" class="link" target="_blank">github <i class="fab fa-github"></i></a>
+
+            </div>
+
+            <div class="social-links">
+                <a href="https://instagram.com/amanullah_a4" class="social-link" target="_blank"><i class="fab fa-instagram"></i></a>
+
+            </div>
+        </article>
+    </section>
+</body>
+
+</html>
